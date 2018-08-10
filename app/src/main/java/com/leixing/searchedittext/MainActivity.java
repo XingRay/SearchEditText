96c099aba98e0146cd369a2cee671afd9bd2f818
leixing1012@gmail.com <wsad7206042457>
2018-08-10 14:32:18 +0800

    增加测试页面

--- app/src/main/java/com/leixing/searchedittext/MainActivity.java
+++ app/src/main/java/com/leixing/searchedittext/MainActivity.java
@@ -1,13 +1,28 @@
 package com.leixing.searchedittext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.leixing.lib.searchbar.OnSearchListener;
+import com.leixing.lib.searchbar.SearchEditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SearchBar sbSearch = findViewById(R.id.sb_search);
+        sbSearch.setAutoSearchEnable(true);
+        sbSearch.setSearchDelayMills(2000);
+        sbSearch.setOnSearchListener(new OnSearchListener() {
+            @Override
+            public void onSearch(SearchEditText.SearchTrigger trigger, String keyword) {
+                Log.i(TAG, "trigger:" + trigger + " keyword" + keyword);
+            }
+        });
     }
 }
